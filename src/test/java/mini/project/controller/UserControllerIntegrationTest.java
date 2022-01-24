c098230c6e2b1aee1525db9e40e4e15c7a41720d
Alwyn-Ong <alwyn.ong.2018@sis.smu.edu.sg>
2022-01-25 01:31:44 +0800

    Added integration tests writing to test db

--- src/test/java/mini/project/controller/UserControllerIntegrationTest.java
+++ src/test/java/mini/project/controller/UserControllerIntegrationTest.java
@@ -4,11 +4,17 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import mini.project.dao.UserDao;
@@ -16,6 +22,7 @@ import mini.project.model.User;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserControllerIntegrationTest {
 
 	@Autowired
@@ -24,11 +31,75 @@ public class UserControllerIntegrationTest {
 	@Autowired
 	private UserDao userDao;
 
+	@BeforeEach
+	void setUp() {
+		userDao.deleteAll();
+		userDao.save(new User("Bob", 2000));
+		userDao.save(new User("Carl", 3000));
+		userDao.save(new User("Dan", 4000));
+		userDao.save(new User("Emily", 3500));
+		userDao.save(new User("Alex", 2500));
+	}
+
+	@AfterEach
+	void tearDown() {
+		userDao.deleteAll();
+	}
+
 	@Test
 	void getUsersWorksThroughAllLayers() throws Exception {
-//		User user = new User("Alice", 1000.00);
-//		userDao.save(user);
-		mockMvc.perform(get("/users").param("min", "1")).andExpect(status().isOk())
-				.andExpect(jsonPath("$.results", Matchers.hasSize(5)));
+		List<User> expectedUsers = userDao.findAll();
+		mockMvc.perform(get("/users")).andExpect(status().isOk())
+				.andExpect(jsonPath("$.results", Matchers.hasSize(expectedUsers.size()))).andDo(result -> {
+
+				});
 	}
+
+	@Test
+	void getUsersWorksThroughAllLayers_withMin() throws Exception {
+		List<User> users = userDao.findAll();
+		double min = 2500.0;
+		List<User> expectedUsers = users.stream().filter(user -> user.getSalary() >= min).collect(Collectors.toList());
+		System.out.println("REULSTSSSSSSSS");
+		mockMvc.perform(get("/users").param("min", Double.toString(min))).andExpect(status().isOk())
+				.andExpect(jsonPath("$.results", Matchers.hasSize(expectedUsers.size()))).andDo(result -> {
+					System.out.println(result);
+				});
+		System.out.println("RESULTSSSSSSSSSSS");
+	}
+
+	@Test
+	void getUsersWorksThroughAllLayers_withMax() throws Exception {
+		List<User> users = userDao.findAll();
+		double max = 3500.0;
+		List<User> expectedUsers = users.stream().filter(user -> user.getSalary() <= max).collect(Collectors.toList());
+		mockMvc.perform(get("/users").param("max", Double.toString(max))).andExpect(status().isOk())
+				.andExpect(jsonPath("$.results", Matchers.hasSize(expectedUsers.size()))).andDo(result -> {
+
+				});
+	}
+
+	@Test
+	void getUsersWorksThroughAllLayers_withLimit() throws Exception {
+		List<User> users = userDao.findAll();
+		int limit = 1;
+		List<User> expectedUsers = users.subList(0, limit);
+		System.out.println(expectedUsers);
+		mockMvc.perform(get("/users").param("limit", Integer.toString(limit))).andExpect(status().isOk())
+				.andExpect(jsonPath("$.results", Matchers.hasSize(expectedUsers.size()))).andDo(result -> {
+
+				});
+	}
+
+	@Test
+	void getUsersWorksThroughAllLayers_withSortName() throws Exception {
+		List<User> users = userDao.findAll();
+		List<User> expectedUsers = users.stream().sorted((user1,user2) -> user1.getName().compareTo(user2.getName())).collect(Collectors.toList());
+		System.out.println(expectedUsers);
+		mockMvc.perform(get("/users").param("min", "1")).andExpect(status().isOk())
+				.andExpect(jsonPath("$.results", Matchers.hasSize(expectedUsers.size()))).andDo(result -> {
+
+				});
+	}
+
 }
