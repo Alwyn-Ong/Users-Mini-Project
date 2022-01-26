449fa56c8e0c25491679319954be1cc7f6a49ef6
Alwyn-Ong <alwyn.ong.2018@sis.smu.edu.sg>
2022-01-26 19:09:26 +0800

    Added swagger ui

--- src/main/java/mini/project/BackendApplication.java
+++ src/main/java/mini/project/BackendApplication.java
@@ -2,6 +2,12 @@ package mini.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +15,13 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+	
+	@Bean
+	public Docket swaggerConfiguration() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("mini.project.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
--- src/main/java/mini/project/dao/UserDao.java
+++ src/main/java/mini/project/dao/UserDao.java
@@ -2,10 +2,12 @@ package mini.project.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 
 import mini.project.model.User;
 
 // To access database
+@Repository
 public interface UserDao extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User> {
 
 	User findByName(String name);
